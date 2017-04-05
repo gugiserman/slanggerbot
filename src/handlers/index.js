ed7c70762cd40a235d93f39e88bdb26a11970414
Gustavo Giserman <gugiserman@gmail.com>
2017-04-05 03:51:22 -0300

    Fix /del command handler file name

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,4 +1,4 @@
 export { default as listHandler } from './list'
 export { default as addHandler } from './add'
-export { default as delHandler } from './remove'
+export { default as delHandler } from './del'
 // export { default as updateHandler } from './update'
