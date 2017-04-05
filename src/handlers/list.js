823c28ff69397b6a80359b93820df8e015b41005
Gustavo Giserman <gugiserman@gmail.com>
2017-04-05 03:34:44 -0300

    Implement pretty /list command

--- src/handlers/list.js
+++ src/handlers/list.js
@@ -1,8 +1,32 @@
+import moment from 'moment'
 import { Slang } from '../db/models'
 
 const listHandler = (context) => {
-  Slang.find().then((slangs) => {
-    context.reply(slangs)
+  const { chat } = context.message
+
+  Slang.find({ 'chat.id': chat.id }).then((slangs) => {
+    const body = slangs.map((slang) => {
+      const {
+        keyword,
+        response,
+        date,
+        author: {
+          first_name,
+          username,
+        },
+      } = slang
+
+      const when = moment(new Date(date)).fromNow()
+      let entry = `<b>${keyword}</b>: ${response} | ${when} by ${first_name} `
+
+      if (username && username.length) {
+        entry += `(@${username}) `
+      }
+
+      return entry
+    }).join("\n")
+
+    context.reply(body)
   })
 }
 
